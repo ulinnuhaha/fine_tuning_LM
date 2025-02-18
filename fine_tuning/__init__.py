144e16155867c047be81d7968e7b3516c9871812
Ulin Nuha <ulinpoenya@gmail.com>
2025-02-18 16:28:36 +0800

    Create __init__.py

---
+++ fine_tuning/__init__.py
@@ -0,0 +1,7 @@
+# few_shot_learning/__init__.py
+
+# Importing specific classes or functions to make them accessible
+from .FTModel import FTModel
+
+# You can also define package-level variables or functions here
+__version__ = "1.0.0"
